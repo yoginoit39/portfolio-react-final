d34e6a0b50f8306fdf1c640aeed5dd4663736ba2
yoginoit39 <ylakhani39@gmail.com>
2022-05-04 18:50:36 +0300

    first commit

---
+++ src/components/WorkData.js
@@ -0,0 +1,63 @@
+export const Work = [
+    {
+        id:1,
+        name:"Tetra Design and Concept",
+        description:"Built with Wordpress Content Management system and Elementor",
+        tags:["Wordpress","Elementor","Graphics Design"],
+        
+        demo:"https://tetradesignandconcepts.com/",
+        //github:"https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id:2,
+        name:"Kirundi Dictionary (Android App)",
+        description:"search filter and a description pop-up is opened by tapping on the word with detailed information",
+        
+        tags:["Kotlin","MySQL","Android Studio"],
+        
+        demo:"https://play.google.com/store/apps/details?id=gateka.armel.kirdictionary",
+        //github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"Visitor Management System (VMS)",
+        description:"Current record system in Uganda is mostly manual based, a visitor is recorded at the front desk using pen and paper, this makes it extremely difficult to track, monitor and retrieve visitor information this web based system helps you to achieve that in a more efficient manner",
+        tags:["Bootstrap","Php","Msql"],
+        
+        demo:"https://github.com/yoginoit39/vms_project.git",
+        //github:"https://github.com/codebucks27/devto-clone"
+    },
+    {
+        id:4,
+        name:"Travel Connection",
+        description:"Uganda is a hot spot tourism destination, millions of tourist's come to the country each year to travel and experience what we have in this amazing  country as its known as the pearl of Africa, this web-based system allows them to book hotels and tour packages more efficiently online. The admin can create and alter these packages as well approve or cancel any booking",
+        tags:["Bootstrap","Php","Msql"],
+        
+        demo:"https://github.com/yoginoit39/Travel-Connection.git",
+       // github:"https://github.com/codebucks27/react-sidebar"
+       
+     },
+    //{
+    //     id:5,
+    //     name:"Todo App(React + Redux)",
+    //     description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    //     tags:["react","redux","framer-motion"],
+        
+    //     demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    //     //github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    // },
+    // {
+    //     id:6,
+    //     name:"Responsive Navigation Menu",
+    //     description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+    //     tags:["react","react-router","css"],
+        
+    //     demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+    //     //github:"https://github.com/codebucks27/React-responsive-navbar"
+        
+    // },
+    
+  
+] 
